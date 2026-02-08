caf7963f0724aa5e049c56f89ea2343728e47f79
harrison <harrisontmhawkins@icloud.com>
2026-02-08 18:01:28 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,4 +2,8 @@ self.addEventListener("install", () => {
   self.skipWaiting();
 });
 
+self.addEventListener("activate", () => {
+  console.log("Service worker active");
+});
+
 self.addEventListener("fetch", () => {});
